654c79bc885f551c0488c7b009c542cbe52f5f4e
duole <13723771404@139.com>
2026-05-28 23:20:35 +0800

    完成所有pwm的配置

--- Src/main.c
+++ Src/main.c
@@ -618,11 +618,11 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 0;
+  htim4.Init.Prescaler = 83;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 65535;
+  htim4.Init.Period = 999;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
   {
     Error_Handler();
@@ -1045,8 +1045,18 @@ void StartDefaultTask(void const * argument)
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);  // ���� CH3
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);  // ���� CH4
 	
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);  // ���� CH3
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_2);  // ���� CH4
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);  // ���� CH3
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);  // ���� CH4
+	
 	__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, 300);  // ռ�ձ� 30%
   __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, 700);  // ռ�ձ� 70%
+	
+  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, 200);  // ռ�ձ� 30%
+  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, 300);  // ռ�ձ� 70%
+	__HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 400);  // ռ�ձ� 30%
+  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_4, 500);  // ռ�ձ� 70%
 	
   /* Infinite loop */
   for(;;)
